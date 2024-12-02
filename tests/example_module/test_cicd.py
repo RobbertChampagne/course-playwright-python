33d52df91d403cb817007304eb24ff3107e2a103
Robbert <robbert.mediahuis@gmail.com>
2024-12-02 07:54:49 +0100

    cicd test 2

--- tests/example_module/test_cicd.py
+++ tests/example_module/test_cicd.py
@@ -1,10 +1,19 @@
 # pytest tests/example_module/test_cicd.py
 from playwright.sync_api import Page, expect
+from core.utils import save_trace
 
 def test_main_navigation(page):
-    page.goto("https://playwright.dev/python/")
-    # Pass:
-    #expect(page.get_by_label("Switch between dark and light")).to_be_visible()
+    context = page.context
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    
+    try:    
+        page.goto("https://playwright.dev/python/")
+       
+        # Pass:
+        #expect(page.get_by_label("Switch between dark and light")).to_be_visible()
 
-    # Fail:
-    expect(page.get_by_label("Switch between dark and light123")).to_be_visible()
+        # Fail:
+        expect(page.get_by_label("Switch between dark and light123")).to_be_visible()
+    
+    finally:
+        save_trace(context, 'cicd', 'cicd_example.zip')
